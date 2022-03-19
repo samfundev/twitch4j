caedcaac402a49bac0b50395a1eb659b5e0dc829
Sidd <iProdigy@users.noreply.github.com>
2022-03-18 22:22:15 -0500

    chore: update ws module build

--- eventsub-websocket/build.gradle.kts
+++ eventsub-websocket/build.gradle.kts
@@ -1,11 +1,14 @@
 // In this section you declare the dependencies for your production and test code
 dependencies {
 	// Twitch4J Modules
-	api(project(":eventsub-common"))
+	api(project(":twitch4j-eventsub-common"))
+}
+
+base {
+	archivesName.set("twitch4j-eventsub-websocket")
 }
 
 publishing.publications.withType<MavenPublication> {
-	artifactId = "twitch4j-eventsub-websocket"
 	pom {
 		name.set("Twitch4J API - EventSub WebSocket Module")
 		description.set("EventSub WebSocket dependency")
